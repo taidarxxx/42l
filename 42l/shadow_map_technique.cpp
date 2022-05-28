c2865807e11ed8ee79c73805ba2c9b63303b5b21
TAIDARXXX <aidar99921@gmail.com>
2022-05-28 18:21:30 +0500

    Добавьте файлы проекта.

---
+++ 42l/shadow_map_technique.cpp
@@ -0,0 +1,45 @@
+#include "shadow_map_technique.h"
+
+
+ShadowMapTechnique::ShadowMapTechnique()
+{
+}
+
+bool ShadowMapTechnique::Init()
+{
+    if (!Technique::Init()) {
+        return false;
+    }
+
+    if (!AddShader(GL_VERTEX_SHADER, "shadow_map.vs")) {
+        return false;
+    }
+
+    if (!AddShader(GL_FRAGMENT_SHADER, "shadow_map.fs")) {
+        return false;
+    }
+
+    if (!Finalize()) {
+        return false;
+    }
+
+    m_WVPLocation = GetUniformLocation("gWVP");
+    m_textureLocation = GetUniformLocation("gShadowMap");
+
+    if (m_WVPLocation == INVALID_UNIFORM_LOCATION ||
+        m_textureLocation == INVALID_UNIFORM_LOCATION) {
+        return false;
+    }
+
+    return true;
+}
+
+void ShadowMapTechnique::SetWVP(const Matrix4f& WVP)
+{
+    glUniformMatrix4fv(m_WVPLocation, 1, GL_TRUE, (const GLfloat*)WVP.m);
+}
+
+void ShadowMapTechnique::SetTextureUnit(unsigned int TextureUnit)
+{
+    glUniform1i(m_textureLocation, TextureUnit);
+}
